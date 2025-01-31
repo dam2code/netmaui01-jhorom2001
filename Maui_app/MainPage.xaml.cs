e5f8a012ffff465dc592df8aa49602cbb6293dca
Jhonatan Romero <jhonatan.romero@educa.madrid.org>
2025-01-31 11:46:09 +0100

    TAREA 2 NOTAS COMPLETO

--- Maui_app/MainPage.xaml.cs
+++ Maui_app/MainPage.xaml.cs
@@ -2,54 +2,36 @@
 {
     public partial class MainPage : ContentPage
     {
+        string nombreArchivo = Path.Combine(FileSystem.AppDataDirectory, "notas.txt");
+        String notaIntroducida;
+
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(nombreArchivo))
+            {
+                editor.Text = File.ReadAllText(nombreArchivo);
+            }
         }
 
-        string numeroTraducido;
-        private void traducir(Object sender, EventArgs e)
+        public void guardar(Object sender, EventArgs e)
         {
-            string numeroIntroducido = NumTelefonoTexto.Text; //OBTENEMOS EL NUMERO RECOGIDO EN EL ENTRY
-            numeroTraducido = Maui_app.PhonewordTranslator.ToNumber(numeroIntroducido); //UTILIZAMOS EL METODO DE LA CLASE
-                                                                                                  //PHONEWORDTRANSLATOR PASANDOLE EL NUMERO INTRODUCIDO
-                                                                                                  //COMPROBAMOS SI EL NUMERO INTRODUCIDO Y TRADUCIDO ESTA VACIO O NO
-            if (!string.IsNullOrEmpty(numeroTraducido))
+            String notaIntroducida = editor.Text;
+
+            if (!string.IsNullOrEmpty(notaIntroducida))
             {
-                //SI CONTIENE ALGO, EL BOTON LLAMAR SE ACTIVA Y SU TEXTO CAMBIA A "LLAMAR A ...."
-                BotonLlamar.IsEnabled = true;
-                BotonLlamar.Text = "Llamar a " + numeroTraducido;
+                this.DisplayAlert("Quieres guardar la nota ?", "", "Si");
             }
             else
             {
-                //SI ESTA VACIO SE DESABILITA EL BOTON LLAMAR Y SE CAMBIA EL TEXTO DEL BOTON
-                BotonLlamar.IsEnabled = false;
-                BotonLlamar.Text = "Llamar";
+                this.DisplayAlert("No has introducido nada", "", "Continuar");
             }
         }
 
-        async void llamada(Object sender, EventArgs e)
+        public void borrar(object sender, EventArgs e)
         {
-            //CUANDO PULSAMOS EL BOTON LLAMAR(SI ESTA HABILITADO) NOS SALE UNA ALERTA QUE NOS PREGUNTA SI QUEREMOS LLAMAR AL NUMERO
-            if (await this.DisplayAlert("Marque un numero", "Te gustaria llamar a " + numeroTraducido + "?", "Si", "No"))
-            {
-                try
-                {
-                    if (PhoneDialer.Default.IsSupported)
-                        PhoneDialer.Default.Open(numeroTraducido);
-                }
-                catch (ArgumentNullException)
-                {
-                    await DisplayAlert("Imposible de marcar", "Numero de telefono no valido", "Vale");
-                }
-                catch (Exception)
-                {
-                    // Other error has occurred.
-                    await DisplayAlert("Imposible de marcar", "Marcado fallido", "Vale");
-                }
-            }
+            editor.Text = "";
         }
-
     }
 
 }
