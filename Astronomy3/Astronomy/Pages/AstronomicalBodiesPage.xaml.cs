fa70f922d5e108efb023dbc211a830bd79053f87
Jhonatan Romero <jhonatan.romero@educa.madrid.org>
2025-02-08 12:19:25 +0100

    TAREA 5 - EJERCICIO 3 - UTILIZACION DE PESTAÑAS Y CLICLED

--- Astronomy3/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
+++ Astronomy3/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -1,10 +1,29 @@
+
 namespace Astronomy.Pages;
 
+[QueryProperty(nameof(AstroName), "astroName")]
 public partial class AstronomicalBodiesPage : ContentPage
 {
 	public AstronomicalBodiesPage()
 	{
 		InitializeComponent();
-
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
     }
+
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+            //UpdateAstroBodyUI(astroName);
+        }
+    }
+
+    
 }
+
